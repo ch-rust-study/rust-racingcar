3086f5c757707deb7a61c400a4c7f46299a441d2
tigger <tigger@channel.io>
2024-04-25 11:45:26 +0900

    feat: 경기 횟수를 받는 기능 추가

--- racingcar/src/main.rs
+++ racingcar/src/main.rs
@@ -9,7 +9,16 @@ fn main() {
     let mut racing_game = RacingGame::new();
 
     let names = input::read_line("차 이름을 입력하세요");
-    racing_game.create_cars(names);
+    racing_game.create_cars(names.trim());
+
+    let rounds_count = input::read_line("경기 횟수를 입력하세요");
+
+    match rounds_count.trim().parse() {
+        Ok(count) => racing_game.set_rounds_count(count),
+        // TODO: handle error
+        Err(e) => panic!("hey!! {}", e)
+    }
+    
 
     let car_names_result = racing_game.get_car_names().join(" ");
     println!("{}", car_names_result);
--- racingcar/src/model/racing_game/mod.rs
+++ racingcar/src/model/racing_game/mod.rs
@@ -2,22 +2,28 @@ use super::car::Car;
 
 pub struct RacingGame {
   cars: Vec<Car>,
+  rounds_count: i32,
 }
 
 impl RacingGame {
   pub fn new() -> RacingGame {
     RacingGame {
-      cars: Vec::new()
+      cars: Vec::new(),
+      rounds_count: 0,
     }
   }
 
-  pub fn create_cars(&mut self, car_names_input: String) {
+  pub fn create_cars(&mut self, car_names_input: &str) {
     // TODO: validation 
     let car_names: Vec<&str> = car_names_input.split(',').collect();
     for name in car_names {
       // TODO: validation
       self.cars.push(Car::new(name.to_string()))
     }
+  }
+
+  pub fn set_rounds_count(&mut self, count: i32) {
+    self.rounds_count = count
   }
 
   pub fn get_car_names(self) -> Vec<String> {
