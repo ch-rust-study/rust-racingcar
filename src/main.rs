f241c3756ea41dbb4de112edd34728b720991b3f
andrew <andrew@channel.io>
2024-04-25 11:18:56 +0900

    feat: impl RacingGame

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ fn is_run() -> bool {
     return rand::thread_rng().gen_range(0..=9) >= 4;
 }
 
+#[derive(Clone)]
 struct Car {
     name: String,
     position: i32,
@@ -58,31 +59,57 @@ impl Car {
     }
 }
 
-fn get_winner_cars(cars: &Vec<Car>) -> String {
-    let max_position = cars.iter().map(|car| car.position).max();
+struct RacingGame {
+    cars: Vec<Car>,
+}
 
-    return cars
-        .iter()
-        .filter(|car| car.position == max_position.unwrap())
-        .map(|car| car.name.as_str())
-        .collect::<Vec<&str>>()
-        .join(", ");
+impl RacingGame {
+    fn new() -> RacingGame {
+        RacingGame { cars: Vec::new() }
+    }
+
+    fn make_cars(&mut self, cars_input: &str) {
+        self.cars = cars_input
+            .split(',')
+            .map(|name| Car::new(name.trim()))
+            .collect()
+    }
+
+    fn start(&mut self, count: i32) {
+        println!("실행 결과\n");
+        for _i in 1..=count {
+            for car in &mut self.cars {
+                car.run();
+                car.log();
+            }
+            println!("");
+        }
+    }
+
+    fn get_winner_cars(&self) -> Vec<Car> {
+        let max_position = self.cars.iter().map(|car| car.position).max();
+
+        self.cars
+            .iter()
+            .filter(|car| car.position == max_position.unwrap())
+            .cloned()
+            .collect()
+    }
 }
 
 fn main() {
-    let cars = input_string("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
-    let count = input_number("시도할 회수는 몇회인가요?");
+    let mut game = RacingGame::new();
+    game.make_cars(&input_string(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분",
+    ));
+    game.start(input_number("시도할 회수는 몇회인가요?"));
 
-    let mut cars: Vec<Car> = cars.split(",").map(|name| Car::new(name.trim())).collect();
-
-    println!("실행 결과\n");
-    for _i in 1..=count {
-        for car in &mut cars {
-            car.run();
-            car.log();
-        }
-        println!("");
-    }
-
-    println!("최종 우승자: {}", get_winner_cars(&cars));
+    println!(
+        "최종 우승자: {}",
+        game.get_winner_cars()
+            .iter()
+            .map(|car| car.name.as_str())
+            .collect::<Vec<&str>>()
+            .join(",")
+    );
 }
